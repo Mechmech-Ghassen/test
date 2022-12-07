e0691405f18519195cc087fba38e49ec4d282a17
Mechmech-Ghassen <ghassen.mechmech@alight.eu>
2022-12-07 15:13:20 +0100

    first commit

---
+++ src/webparts/projetWebPart/components/IProjetWebPartProps.ts
@@ -0,0 +1,7 @@
+export interface IProjetWebPartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/projetWebPart/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IProjetWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ProjetWebPartWebPartStrings' {
+  const strings: IProjetWebPartWebPartStrings;
+  export = strings;
+}
